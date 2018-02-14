43a7bc034101019ad8a8d564990d7e574747af55
Tayef <Tayef@10.0.0.246>
2018-02-13 22:48:48 -0500

    page changes

--- framWork/src/test/java/applicationPages/LoginPage.java
+++ framWork/src/test/java/applicationPages/LoginPage.java
@@ -5,24 +5,30 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
+import helper.Utility;
+
 public class LoginPage
 {
 	WebDriver driver;
+	public LoginPage(WebDriver ldriver) {
+		this.driver=ldriver;
+	}
+	
 	@FindBy(how = How.ID, using = "txtUsername")WebElement userName;
 	@FindBy(how = How.ID, using = "txtPassword")WebElement password;
 	@FindBy(how = How.ID, using = "btnLogin")WebElement loginButton;
 	
 	public void enterUserName(String username) 
 	{
-		userName.sendKeys(username);
+		Utility.waitForWebElement(driver, userName, 1).sendKeys(username);
 	}
 	public void enterPassword(String pass) 
 	{
-		password.sendKeys(pass);
+		Utility.waitForWebElement(driver, password, 1).sendKeys(pass);
 	}
 	public void clickOnLoginButton() 
 	{
-		loginButton.click();
+		Utility.waitForWebElement(driver, loginButton, 2).click();
 	}
 	public void loginToApplication(String username,String pass) 
 	{
