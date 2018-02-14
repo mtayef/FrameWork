b275fe81ae45b70dd1adcb1de12a5515cf3d130a
mtayef <30615194+mtayef@users.noreply.github.com>
2018-02-14 17:06:20 -0500

    Update UserPage.java

--- framWork/src/test/java/applicationPages/UserPage.java
+++ framWork/src/test/java/applicationPages/UserPage.java
@@ -2,8 +2,6 @@ package applicationPages;
 
 import java.util.List;
 
-//import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
