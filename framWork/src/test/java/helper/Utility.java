310f674ba5f4697e72dfc190f2ceb9fbb23fb077
Tayef <Tayef@10.0.0.246>
2018-02-16 19:49:57 -0500

    Adding lib

--- framWork/src/test/java/helper/Utility.java
+++ framWork/src/test/java/helper/Utility.java
@@ -1,11 +1,15 @@
 package helper;
 
 import java.io.File;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -13,6 +17,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
 
 
 public class Utility {
@@ -67,5 +73,138 @@ public class Utility {
 		highLightElement(driver, element);
 
 		return element;
+	}
+	
+	public static void verifyBrokenLink(String linkurl) {
+		URL url = null;
+		HttpURLConnection connection = null;
+
+		try {
+			url = new URL(linkurl);
+		} catch (Exception e) {
+			System.out.println("URL is not configured");
+		}
+
+		try {
+			connection = (HttpURLConnection) url.openConnection();
+
+			connection.setConnectTimeout(5000);
+
+			connection.connect();
+
+		} catch (Exception e) {
+
+		}
+
+		int code = 0;
+		try {
+			code = connection.getResponseCode();
+		} catch (Exception e) {
+
+		}
+
+		System.out.println("Response Code from Server is " + code);
+
+		Assert.assertEquals(code, 200);
+	}
+
+	public static void verifyMultipleBrokenLink(WebDriver ldriver, String xpath) {
+
+		SoftAssert soft = new SoftAssert();
+		URL url = null;
+		HttpURLConnection connection = null;
+
+		List<WebElement> allLinks = ldriver.findElements(By.xpath(xpath));
+
+		System.out.println("Total link to verify " + allLinks.size());
+
+		for (WebElement ele : allLinks) {
+
+			String hrefurl = ele.getAttribute("href");
+
+			System.out.println("Testing link with this url " + hrefurl);
+
+			try {
+				url = new URL(hrefurl);
+			} catch (Exception e) {
+				System.out.println("URL is not configured");
+			}
+
+			try {
+				connection = (HttpURLConnection) url.openConnection();
+
+				connection.setConnectTimeout(5000);
+
+				connection.connect();
+
+			} catch (Exception e) {
+
+			}
+
+			int code = 0;
+			try {
+				code = connection.getResponseCode();
+			} catch (Exception e) {
+
+			}
+
+			System.out.println("Response Code from Server is " + code);
+			soft.assertEquals(code, 200);
+
+		}
+
+		soft.assertAll();
+
+	}
+
+	public static void verifyMultipleBrokenImages(WebDriver ldriver, String xpath) {
+
+		SoftAssert soft = new SoftAssert();
+		URL url = null;
+		HttpURLConnection connection = null;
+
+		List<WebElement> allLinks = ldriver.findElements(By.xpath(xpath));
+
+		System.out.println("Total Images to verify " + allLinks.size());
+
+		for (WebElement ele : allLinks) {
+
+			String hrefurl = ele.getAttribute("src");
+
+			System.out.println("Testing Images with this url " + hrefurl);
+
+			try {
+				url = new URL(hrefurl);
+			} catch (Exception e) {
+				System.out.println("URL is not configured");
+			}
+
+			try {
+				connection = (HttpURLConnection) url.openConnection();
+
+				connection.setConnectTimeout(5000);
+
+				connection.connect();
+
+			} catch (Exception e) {
+
+			}
+
+			int code = 0;
+			try {
+				code = connection.getResponseCode();
+
+			} catch (Exception e) {
+
+			}
+
+			System.out.println("Response Code from Server is " + code);
+
+			soft.assertEquals(code, 200);
+
+		}
+
+		soft.assertAll();
+
 	}
 }
